d254c8b7918bddfaae942a2b3411d95044e0ea0c
shelleyyyyy <shelleywr23@mail.vmi.edu>
2022-10-02 14:53:11 -0400

    finished

---
+++ open.py
@@ -0,0 +1,8 @@
+from location import Location
+
+class Open:
+    def __init__(self, id):
+        self.location = Location()
+        self.percept = "open"
+        self.id = id
+
---
+++ test.py
@@ -0,0 +1,9 @@
+from blocked import Blocked
+from cell import Cell
+
+c = Cell()
+
+c.type = Blocked(1)
+
+
+c.toString()
