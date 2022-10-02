19c8f5211b19610cdeb1f020287eaafb26104731
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-10-02 15:15:16 -0400

    made action and move agent along with a test for move agent to get costs from different moves

---
+++ Action.py
@@ -0,0 +1,15 @@
+costN = 1
+costS = 1
+costE = 1
+costW = 1
+
+def getCosts(action):
+    if action == 'north':
+        return costN
+    elif action == 'south':
+        return costS
+    elif action == 'west':
+        return costW
+    elif action == 'east':
+        return costE
+    return 'incorrect input'
