8cc2bc563ca63eff4a32835147e8d4496ac5221a
gilmanja23 <gilmanja23@mail.vmi.edu>
2022-10-09 19:37:50 -0400

    create node+successor function

---
+++ node.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, state, parent=None, action=None):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.actionsList = []
+        self.pathCost = 0 if action is None and parent is None else self.parent.getPathCost() + self.action.getPathCost(action)
+
+    class NodeComparator:
+        def __init__(self, goals, searchType):
+            self.goals = goals
+            self.searchType = searchType
+
+        def compareType(self, searchType):
+            match searchType:
+                case "UniformedCostSearch":
+                    #do 
+                case "GreedyBestFirstSearch":
+
+                case "AStarSearch":
---
+++ successorFunction.py
@@ -0,0 +1,15 @@
+
+class SuccessorFunction:
+    def __init__(self):
+        pass
+
+    def expand(self, node, environment):
+
+    def successorFunction(self, parent, aciton, environment):
+
+    def goForward(self, environment, state):
+    
+    def turnRight(self, state):
+
+    def turnLeft(self, state):
+        
