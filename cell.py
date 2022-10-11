cbb7366bbecc7ce82873972a550bb7cc63d0eec4
BoleBorson <corsonca23@mail.vmi.edu>
2022-10-11 13:07:35 -0400

    more tweaks to successor

--- cell.py
+++ cell.py
@@ -4,12 +4,12 @@ from constants import constants
 from open import Open
 
 class Cell:
-    def __init__(self, x, y, direction, containsTruck=False, blocked=False, cell_type=constants.BLANK, percepts=Percept()):
+    def __init__(self, x, y, direction=constants['FACING_EAST'], containsTruck=False, cell_type=constants['BLANK'], percepts=Percept()):
         self.location = Location(x, y)
         self.direction = direction
         self.cell_type = cell_type
-        self.containsTruck = truck
-        self.percepts = percept
+        self.containsTruck = containsTruck
+        self.percepts = percepts
 
     def toString(self):
         print("----------Cell----------")
--- successorFunction.py
+++ successorFunction.py
@@ -1,64 +1,11 @@
 import collections
 from location import Location
+from constants import constants
 from node import Node
   
 class SuccessorFunction:
     def __init__(self):
         pass
-
-    def expand(self, node, environment):
-        list = collections.deque()
-
-        parentNode = node.copyOfNode()
-
-        try:
-            list.append(SuccessorFunction(parentNode, GO_FORWARD, environment))
-        except:
-            pass
-
-        parentNode = node.copyOfNode()
-        list.append(SuccessorFunction(parentNode, GO_RIGHT, environment))
-        parentNode = node.copyOfNode()
-        list.append(SuccessorFunction(parentNode, GO_LEFT, environment))
-
-        return list
-
-    def successorFunction(self, parent, action, environment):
-        state = parent.state.type #state is a Cell Type
-
-        shiftXValue = 0
-        shiftYValue = 0
-
-        if state.direction == FACING_SOUTH:
-            shiftXValue = MOVING_SOUTH
-        elif state.direction == FACING_WEST:
-            shiftYValue = MOVING_WEST
-        elif state.direction == MOVING_NORTH:
-            shiftXValue = MOVING_NORTH
-        else:
-            shiftYValue = MOVING_EAST
-        
-        if action == GO_FORWARD:
-            if goForward(environment, state):
-                i = state.location.x + shiftXValue
-                j = state.location.y + shiftXValue
-
-                if i < 0 or i >= len(environment.trucks) or j < 0 or j >= len(environment.trucks):
-                    return None
-                
-                #potentially need to set the percepts of the cell it moves to 
-                state.location = Location(i, j)
-            else:
-                return None
-        elif action == TURN_RIGHT:
-            turnRight(state)
-        elif action == TURN_LEFT:
-            turnLeft(state)
-        else:
-            pass
-
-        node = Node(state=state, parent=parent, action=action)
-        return node
 
     def goForward(self, environment, state):
 
@@ -79,11 +26,64 @@ class SuccessorFunction:
 
         if i < 0 or i >= len(environment.trucks) or j < 0 or j >= len(environment.trucks):
             return False
-        
-
-
 
     def turnRight(self, state):
         pass
     def turnLeft(self, state):
         pass
+
+    def expand(self, node, environment):
+        list = collections.deque()
+
+        parentNode = node.copyOfNode()
+
+        try:
+            list.append(SuccessorFunction(parentNode, constants["GO_FORWARD"], environment))
+        except:
+            pass
+
+        parentNode = node.copyOfNode()
+        list.append(SuccessorFunction(parentNode, GO_RIGHT, environment))
+        parentNode = node.copyOfNode()
+        list.append(SuccessorFunction(parentNode, GO_LEFT, environment))
+
+        return list
+
+    def successorFunction(self, parent, action, environment):
+        state = parent.state.type #state is a Cell Type
+
+        shiftXValue = 0
+        shiftYValue = 0
+
+        if state.direction == constants['FACING_SOUTH']:
+            shiftXValue = constants['MOVING_SOUTH']
+        elif state.direction == constants['FACING_WEST']:
+            shiftYValue = constants['MOVING_WEST']
+        elif state.direction == constants['MOVING_NORTH']:
+            shiftXValue = constants['MOVING_NORTH']
+        else:
+            shiftYValue = constants['MOVING_EAST']
+        
+        if action == constants['GO_FORWARD']:
+            if self.goForward(environment, state):
+                i = state.location.x + shiftXValue
+                j = state.location.y + shiftYValue
+
+                if i < 0 or i >= len(environment.trucks) or j < 0 or j >= len(environment.trucks):
+                    return None
+                
+                #potentially need to set the percepts of the cell it moves to 
+                state.location = Location(i, j)
+            else:
+                return None
+        elif action == TURN_RIGHT:
+            self.turnRight(state)
+        elif action == TURN_LEFT:
+            self.turnLeft(state)
+        else:
+            pass
+
+        node = Node(state=state, parent=parent, action=action)
+        return node
+
+    
