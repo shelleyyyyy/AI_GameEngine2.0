7e1e6a5f821f2418ade45a438cc5887a75ecc504
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-10-31 18:33:04 -0400

    idls

--- test_dls.py
+++ test_dls.py
@@ -1,8 +1,10 @@
 from environment import Environment
 from searchAlgorithms.dls_cam import depthLimitedSearch
+from searchAlgorithms.idls_cam import iterativeDepthLimitedSearch
 
 e = Environment()
 
+#s = depthLimitedSearch(e,e.cells[2][3], 7)
 s = depthLimitedSearch(e,e.cells[2][3], 7)
-
 print(s)
+
