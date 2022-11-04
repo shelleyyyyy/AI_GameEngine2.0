b78a92976545bb0a871da54722b07f7104dcccad
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-11-04 15:08:39 -0400

    .

--- backend/searchAlgorithms/depthFirstSearch.py
+++ backend/searchAlgorithms/depthFirstSearch.py
@@ -10,18 +10,19 @@ def depthFirstSearch(environment: Environment, root: Cell):
     node = Node(state = root)
     if node.state.cell_type == constants["GOAL_CELL"]:
         return node.actionsList
-    
+    fIndex = 0
+    eIndex = 0
     frontier = [node]
-    explored = []
-
+    explored = dict()
+    
     while True:
         if len(frontier) == 0:
             return []
         shallow = frontier.pop(len(frontier)-1)
-        explored.append(shallow)
-        
+        #explored.append(shallow)
+        #explored[shallow] = {shallow.state.location.x, shallow.state.location.y, shallow.state.direction}
+        explored[{shallow.state.location.x, shallow.state.location.y, shallow.state.direction}] = shallow
         sucFunc = SuccessorFunction()
-
         expand = sucFunc.expand(node = shallow, environment = environment)
         
         for n in expand:
@@ -34,11 +35,11 @@ def depthFirstSearch(environment: Environment, root: Cell):
 
 
 def checklist(node, frontier, explored):
+    key = {node.state.location.x, node.state.location.y, node.state.direction}
     for n in frontier:
         if n.state.location.x == node.state.location.x and n.state.location.y == node.state.location.y and n.state.direction == node.state.direction:
             return True
-    for n in explored:
-        if n.state.location.x == node.state.location.x and n.state.location.y == node.state.location.y and n.state.direction == node.state.direction:
+    if key in explored.keys():
             return True
     return False
 
--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rasco\Documents\GitHub\AI_GameEngine2.0\backend\venv
+set VIRTUAL_ENV=C:\Users\cammi\OneDrive\Documents\GitHub\AI_GameEngine2.0\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
