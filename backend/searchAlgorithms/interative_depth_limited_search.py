4f7f0dbf9a79c590422d60b27719bb46826ced02
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-11-08 16:27:09 -0500

    added hash of explored

--- backend/searchAlgorithms/interative_depth_limited_search.py
+++ backend/searchAlgorithms/interative_depth_limited_search.py
@@ -10,13 +10,13 @@ def iterativeDepthLimitedSearch(environment, root, limit, t):
         return node.actionsList
     
     frontier = [node]
-    explored = []
+    explored = dict()
     depth = 0
     while True:
         if len(frontier) == 0:
             return []
         shallow = frontier.pop(len(frontier)-1)
-        explored.append(shallow)
+        explored[shallow.state.location.x, shallow.state.location.y, shallow.state.direction] = shallow
         
         sucFunc = SuccessorFunction()
         if depth != limit:
@@ -34,14 +34,13 @@ def iterativeDepthLimitedSearch(environment, root, limit, t):
 
 
 def checklist(node, frontier, explored):
+    key = node.state.location.x, node.state.location.y, node.state.direction
     for n in frontier:
         if n.state.location.x == node.state.location.x and n.state.location.y == node.state.location.y and n.state.direction == node.state.direction:
             return True
-    for n in explored:
-        if n.state.location.x == node.state.location.x and n.state.location.y == node.state.location.y and n.state.direction == node.state.direction:
+    if key in explored.keys():
             return True
     return False
-
 def checkGoalState(node):
     if node.state == constants['GOAL_CELL']:
         return
