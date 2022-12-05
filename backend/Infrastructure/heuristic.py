d77bca8d40020d367b2f1ee3c8f28ba6c129aae8
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-12-05 14:02:02 -0500

    heuristics

--- backend/Infrastructure/heuristic.py
+++ backend/Infrastructure/heuristic.py
@@ -6,18 +6,39 @@ from Infrastructure.node import Node
 from gameEngine.truckAgent import TruckAgent
 import math
 
-def hueristics(truck: TruckAgent, environment: Environment):
+class Heuristics():
 
-    tl = truck.location
-    goals: Cell = environment.goals
-    hur = -1
-    for g in goals:
-        x = math.pow((tl.x - g.location.x), 2)
-        y = math.pow((tl.y - g.location.y), 2)
-        p = math.sqrt(x+y)
-        if hur == -1:
-            hur.append(g)
-        elif hur > p:
-            hur = g
+    def __init__(self):
+        pass
         
-    return hur
+    def start_hueristic(truck: TruckAgent, environment: Environment):
+
+        tl = truck.location
+        goals: Cell = environment.goals
+        hur = []
+        for g in goals:
+            x = math.pow((tl.x - g.location.x), 2)
+            y = math.pow((tl.y - g.location.y), 2)
+            p = math.sqrt(x+y)
+            if g.found == True:
+                continue
+            if len(hur) != 0:
+                hur.append(g)
+            elif hur[0] > p:
+                hur.insert(0, g)
+            else:
+                hur.append(g)
+
+        return hur
+
+    def check_if_found(hur):
+        if hur[0].found == True:
+           hur.pop()
+        return(hur)
+
+    def check_heuristic(truck: TruckAgent, goal: Cell):
+        x = math.pow((truck.location.x - goal.location.x), 2)
+        y = math.pow((truck.location.y - goal.location.y), 2)
+        p = math.sqrt(x+y)
+        
+        return p
