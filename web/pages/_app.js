5a7d7ba3bfa3c1f44a34feb37875e5554b2118aa
shelleyyyyy <shelleywr23@mail.vmi.edu>
2022-12-07 19:32:21 -0500

    asfd

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div data-theme="luxury" className=" pb-96">
+    <div data-theme="night" className=" pb-96">
       <Layout>
       <Component {...pageProps} />
     </Layout> 
